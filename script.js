cdcdcbeeb8e5adfbe60c222340d8184072b25525
Dimovski <aleksandar.dimovski@torontomu.ca>
2024-02-26 14:23:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function promptForName() {
         document.getElementById("username").innerText = "Hello, " + name + "!";
     }
 }
+
+function enlargeImage() {
+    var img = document.getElementById("myImage");
+    // Change the width of the image to enlarge it
+    img.style.width = (parseInt(img.style.width) || img.width) * 2 + "px";
+}
