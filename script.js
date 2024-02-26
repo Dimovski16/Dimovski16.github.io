fe697ac683c2700c102159832bbc8549d3387984
Dimovski <aleksandar.dimovski@torontomu.ca>
2024-02-26 14:32:10 -0500

    Commit

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ function promptForName() {
         document.getElementById("username").innerText = "Hello, " + name + "!";
     }
 }
-
 function enlargeImage() {
     var img = document.getElementById("myImage");
-    // Change the width of the image to enlarge it
     img.style.width = (parseInt(img.style.width) || img.width) * 2 + "px";
+}
+document.getElementById("menu-item-1").addEventListener("click", function() {
+    window.location.href = "page1.html";
+});
+
+document.getElementById("menu-item-2").addEventListener("click", function() {
+    window.location.href = "page2.html";
+});
+
+var navItems = document.getElementsByClassName("nav-item");
+for (var i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener("mouseover", function() {
+        this.style.backgroundColor = "lightgray";
+    });
+    navItems[i].addEventListener("mouseout", function() {
+        this.style.backgroundColor = "transparent";
+    });
 }
