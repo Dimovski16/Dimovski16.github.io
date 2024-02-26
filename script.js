0b22fa107820d1f860980379fde77ee38efce3e2
Dimovski <aleksandar.dimovski@torontomu.ca>
2024-02-26 16:33:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,23 +5,6 @@ function promptForName() {
     }
 }
 function enlargeImage() {
-    var img = document.getElementById("myImage");
+    var img = document.getElementById("AD16");
     img.style.width = (parseInt(img.style.width) || img.width) * 2 + "px";
-}
-document.getElementById("menu-item-1").addEventListener("click", function() {
-    window.location.href = "page1.html";
-});
-
-document.getElementById("menu-item-2").addEventListener("click", function() {
-    window.location.href = "page2.html";
-});
-
-var navItems = document.getElementsByClassName("nav-item");
-for (var i = 0; i < navItems.length; i++) {
-    navItems[i].addEventListener("mouseover", function() {
-        this.style.backgroundColor = "lightgray";
-    });
-    navItems[i].addEventListener("mouseout", function() {
-        this.style.backgroundColor = "transparent";
-    });
 }
