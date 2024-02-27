f719e68bd16c55a6b815603fdd5e8615c9423d6a
Dimovski <aleksandar.dimovski@torontomu.ca>
2024-02-26 21:52:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,6 @@
     }
   }
   
-
   function enlargeImage() {
     var image = document.getElementById("AD16");
     if (image.style.width === "200px") {
@@ -19,11 +18,22 @@
 
   function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show"); }
+
     window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content"); for (var i = 0; i < dropdowns.length; i++) {
-    var openDropdown = dropdowns[i];
-    if (openDropdown.classList.contains('show')) {
-    openDropdown.classList.remove('show'); }
-    } }
+      if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content"); 
+        for (var i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show'); }
+      }  
+     }
     }
+    function mouseOverNav(element) {
+      element.style.color = "FF0000";
+      element.style.backgroundColor = "transparent";
+  }
+  
+  function mouseOutNav(element) {
+      element.style.color = "0000FF"; 
+      element.style.backgroundColor = "transparent"; }
